912f40e8747921389864e6748b17e8c57600d3e3
David Pivik <96999894+HyperNylium@users.noreply.github.com>
2025-03-22 03:52:02 -0400

    Some fixes
    1. Adding a ton of logging for easier debugging.
    2. Reworked the "load_queue()" function.
    3. Other small things.

--- app/app.py
+++ app/app.py
@@ -9,18 +9,25 @@ from appdata.modules.Vars import check_dependencies, update_mdnx_config, update_
 
 
 def app():
+    logger.info("[app] Initializing MDNX API...")
     mdnx_api = MDNX_API(mdnx_path=MDNX_SERVICE_BIN_PATH)
+    logger.info("[app] MDNX API initialized.")
 
     # Authenticate with MDNX service if needed or force auth if user wants to
+    logger.info("[app] Checking to see if user is authenticated with MDNX service (cr_token.yml exists?)...")
     if not os.path.exists(MDNX_SERVICE_CR_TOKEN_PATH) or config["app"]["MDNX_API_FORCE_REAUTH"] == True:
         mdnx_api.auth()
 
         # update the "MDNX_API_FORCE_REAUTH" config to False
+        logger.info("[app] Checking to see if user wants to force re-auth with MDNX service...")
         if config["app"]["MDNX_API_FORCE_REAUTH"] == True:
             update_app_config("MDNX_API_FORCE_REAUTH", False)
+        else:
+            logger.info("[app] User does not want to force re-auth with MDNX service.")
+    else:
+        logger.info("[app] User is already authenticated with MDNX service, and cr_token.yml exists.")
 
-
-    # Get the current queue
+    # Get the current queue IDs
     queue_output = mdnx_api.queue_manager.output()
     if queue_output is not None:
         queue_ids = tuple(queue_output.keys())
@@ -31,9 +38,11 @@ def app():
     if len(config["monitor-series-id"]) != 0:
         for id in config["monitor-series-id"]:
             if id not in queue_ids:
+                logger.info(f"[app] Starting to monitor series with ID: {id}")
                 mdnx_api.start_monitor(id)
             else:
-                logger.info(f"Series with ID: {id} is already being monitored.")
+                logger.info(f"[app] Series with ID: {id} is already being monitored. Updating with new data...")
+                mdnx_api.update_monitor(id)
 
     # Stop monitoring series that are not in the config file
     if len(queue_ids) != 0:
@@ -43,7 +52,7 @@ def app():
 
 
 if __name__ == "__main__":
-    logger.info("MDNX-auto-dl has started.")
+    logger.info("[app] MDNX-auto-dl has started.")
     check_dependencies()
     update_mdnx_config()
     app()
--- app/appdata/modules/QueueManager.py
+++ app/appdata/modules/QueueManager.py
@@ -11,58 +11,70 @@ class QueueManager:
         self.queue_path = queue_path
         self.queue_data = self.load_queue()
 
-    def load_queue(self):
+    def load_queue(self) -> dict:
         if os.path.exists(self.queue_path):
             try:
+                logger.info(f"[QueueManager] Loading queue from {self.queue_path}.")
                 with open(self.queue_path, "r", encoding="utf-8") as data:
-                    return json.load(data)
+                    queue_data = json.load(data)
+                    logger.info(f"[QueueManager] Queue loaded from {self.queue_path}.")
+                    return queue_data
             except json.JSONDecodeError:
-                logger.error("Malformed JSON in queue file. Starting with an empty queue.")
-                return {}
+                logger.error("[QueueManager] Malformed JSON in queue file. Starting with an empty queue.")
+            except Exception as e:
+                logger.error(f"[QueueManager] Error loading queue. Starting with an empty queue.\n{e}")
         else:
-            os.makedirs(os.path.dirname(self.queue_path), exist_ok=True)
+            logger.info(f"[QueueManager] Queue file not found at {self.queue_path}. Starting with an empty queue.")
+
+        # Create an empty queue file if it doesn't exist
+        with open(self.queue_path, "a", encoding="utf-8") as empty_queue_file:
+            empty_queue_file.write("{}")
+
         return {}
 
-    def save_queue(self):
+    def save_queue(self) -> None:
+        logger.info("[QueueManager] Saving queue.")
         with open(self.queue_path, "w", encoding="utf-8") as f:
             json.dump(self.queue_data, f, indent=4, ensure_ascii=False)
-        logger.info(f"Queue saved to {self.queue_path}.")
+        logger.info(f"[QueueManager] Queue saved to {self.queue_path}.")
 
     def add(self, new_data: dict):
+        logger.info("[QueueManager] Adding series to the queue.")
         for series_id, series_info in new_data.items():
             if series_id in self.queue_data:
-                # Update existing entry:
+                # Update existing entry with new data
                 self.queue_data[series_id]["series"] = series_info["series"]
                 self.queue_data[series_id]["seasons"].update(series_info["seasons"])
                 self.queue_data[series_id]["episodes"].update(series_info["episodes"])
-                logger.info(f"Updated series '{series_id}' in the queue.")
+                logger.info(f"[QueueManager] Updated series '{series_id}' in the queue.")
             else:
-                # Add a new entry:
+                # Add a new entry to the queue
                 self.queue_data[series_id] = series_info
-                logger.info(f"Added series '{series_id}' to the queue.")
+                logger.info(f"[QueueManager] Added series '{series_id}' to the queue.")
         self.save_queue()
 
-    def remove(self, series_id: str):
+    def remove(self, series_id: str) -> None:
+        logger.info(f"[QueueManager] Removing series {series_id} from the queue.")
         if series_id in self.queue_data:
             del self.queue_data[series_id]
             self.save_queue()
-            logger.info(f"Removed series '{series_id}' from the queue.")
+            logger.info(f"[QueueManager] Removed series '{series_id}' from the queue.")
         else:
-            logger.warning(f"Series '{series_id}' not found in the queue.")
+            logger.warning(f"[QueueManager] Series '{series_id}' not found in the queue.")
 
-    def update_episode_status(self, series_id: str, episode_id: str, status: bool):
+    def update_episode_status(self, series_id: str, episode_id: str, status: bool) -> None:
         if series_id not in self.queue_data:
-            logger.warning(f"Series '{series_id}' not found in the queue.")
+            logger.warning(f"[QueueManager] Series '{series_id}' not found in the queue.")
             return
 
         episodes = self.queue_data[series_id].get("episodes", {})
         if episode_id not in episodes:
-            logger.warning(f"Episode '{episode_id}' not found in series '{series_id}'.")
+            logger.warning(f"[QueueManager] Episode '{episode_id}' not found in series '{series_id}'.")
             return
 
         episodes[episode_id]["episode_downloaded"] = status
         self.save_queue()
-        logger.info(f"Updated episode '{episode_id}' in series '{series_id}' to downloaded={status}.")
+        logger.info(f"[QueueManager] Updated episode '{episode_id}' in series '{series_id}' to downloaded={status}.")
 
-    def output(self):
+    def output(self) -> dict | None:
         return self.queue_data if self.queue_data else None
