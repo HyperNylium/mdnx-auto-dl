acb71808f3019af2dd6814ec5be7a9cd97d1d3a4
HyperNylium <hypernylium@hnhomelab.com>
2025-07-22 19:08:52 -0400

    First try at ntfy support

--- app/appdata/modules/Notification.py
+++ app/appdata/modules/Notification.py
@@ -1,31 +1,49 @@
-# Send SMTP notification when new episode is downloaded for a series
-# This is a quick draft of what i am thinking and needs improvement
-# NOT PROD READY!!!
 
-import smtplib
-from .Vars import logger
+import subprocess
+from .Vars import logger, config
 
 
 
-class Notification:
-    def __init__(self, email, password, smtp_server, smtp_port, smtp_tls=True, smtp_ssl=False):
-        self.email = email
-        self.password = password
-        self.smtp_server = smtp_server
-        self.smtp_port = smtp_port
-        self.smtp_tls = smtp_tls
-        self.smtp_ssl = smtp_ssl
-    
-    def send_email(self, to, subject, body):
+class ntfy:
+    def __init__(self):
+        self.ntfy_script_path = config["app"]["NTFY_SCRIPT_PATH"]
+
+    def notify(self, message):
         try:
-            server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-            if self.smtp_tls:
-                server.starttls()
-            if self.smtp_ssl:
-                server.login(self.email, self.password)
-            server.sendmail(self.email, to, f"Subject: {subject}\n\n{body}")
-            server.quit()
+            logger.info(f"[Notification][ntfy] Sending notification: {message}")
+            subprocess.run([self.ntfy_script_path, message], check=True)
         except Exception as e:
-            logger.info(f"[Notification] Error sending email: {e}")
+            logger.info(f"[Notification][ntfy] Error sending notification: {e}")
             return False
         return True
+
+
+
+
+
+
+
+
+# import smtplib
+# class Notification:
+#     def __init__(self, email, password, smtp_server, smtp_port, smtp_tls=True, smtp_ssl=False):
+#         self.email = email
+#         self.password = password
+#         self.smtp_server = smtp_server
+#         self.smtp_port = smtp_port
+#         self.smtp_tls = smtp_tls
+#         self.smtp_ssl = smtp_ssl
+    
+#     def send_email(self, to, subject, body):
+#         try:
+#             server = smtplib.SMTP(self.smtp_server, self.smtp_port)
+#             if self.smtp_tls:
+#                 server.starttls()
+#             if self.smtp_ssl:
+#                 server.login(self.email, self.password)
+#             server.sendmail(self.email, to, f"Subject: {subject}\n\n{body}")
+#             server.quit()
+#         except Exception as e:
+#             logger.info(f"[Notification] Error sending email: {e}")
+#             return False
+#         return True
