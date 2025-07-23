4f9004cc14987142af4ec318084ddf40a9f21664
HyperNylium <96999894+HyperNylium@users.noreply.github.com>
2025-07-22 21:12:21 -0400

    Add SMTP support

--- appdata/config/ntfy.sh
+++ appdata/config/ntfy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Your ntfy instance URL
-NTFY_URL="https://notify.yourdomain.com/somecoolroom"
+NTFY_URL="https://notify.yourdomain.com/coolstuff"
 
 exec curl -d "$@" "$NTFY_URL"
