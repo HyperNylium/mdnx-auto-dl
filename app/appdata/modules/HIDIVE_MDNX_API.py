44b0b868c89913128fab4f5b9827c3b4f767682a
HyperNylium <96999894+HyperNylium@users.noreply.github.com>
2025-09-28 12:12:03 -0400

    Forgot to do the other side lol

--- app/appdata/modules/HIDIVE_MDNX_API.py
+++ app/appdata/modules/HIDIVE_MDNX_API.py
@@ -459,7 +459,7 @@ class HIDIVE_MDNX_API:
                 cleaned = line.rstrip()
                 logger.info(f"[HIDIVE_MDNX_API][multi-downloader-nx] {cleaned}")
 
-                if any(ok_log in cleaned.lower() for ok_log in MDNX_API_OK_LOGS):
+                if any(ok_log.lower() in cleaned.lower() for ok_log in MDNX_API_OK_LOGS):
                     success = True
 
         if proc.returncode != 0:
