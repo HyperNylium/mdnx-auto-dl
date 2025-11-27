0721ab46b88405665a5373b8489037ca99b51b20
HyperNylium <hypernylium@hnhomelab.com>
2025-11-26 22:09:41 -0500

    Dependency update
    Dependency updates:
    Updated multi-download-nx from 5.6.5 -> 5.6.6

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from appdata.modules.Vars import (
     update_mdnx_config, update_app_config, handle_exception, get_running_user, output_effective_config
 )
 
-__VERSION__ = "2.2.1"
+__VERSION__ = "2.2.2"
 
 
 def app():
