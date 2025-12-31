2baae38b981410f1508e0d06ea0b43e703400818
HyperNylium <96999894+HyperNylium@users.noreply.github.com>
2025-12-31 00:01:09 -0500

    Inc version

--- app/app.py
+++ app/app.py
@@ -11,7 +11,7 @@ from appdata.modules.Vars import (
     update_mdnx_config, update_app_config, handle_exception, get_running_user, output_effective_config
 )
 
-__VERSION__ = "2.2.6"
+__VERSION__ = "2.3.0"
 
 
 def app():
