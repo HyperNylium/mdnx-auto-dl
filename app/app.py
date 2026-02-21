639796ebba96e7386700b6085b8723fdb341b9d2
HyperNylium <hypernylium@hnhomelab.com>
2026-02-21 15:26:44 +0300

    Downgrade mdnx to v5.6.8

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ from appdata.modules.Vars import (
     update_mdnx_config, update_app_config, handle_exception, get_running_user, output_effective_config
 )
 
-__VERSION__ = "2.3.4-dev"
+__VERSION__ = "2.3.4-dev-2"
 
 
 def app():
