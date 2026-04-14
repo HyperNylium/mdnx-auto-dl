047f0ab06664b72df39c54b5abb46aa00975c5cd
HyperNylium <96999894+HyperNylium@users.noreply.github.com>
2026-04-13 22:42:56 -0400

    Add required symlinks to ZLOs static folder

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -141,6 +141,13 @@ fi
 chown -R "$USER_ID:$GROUP_ID" /app
 chmod -R 775 /app
 
+# Make required symlinks for ZLO7 support
+ln -sfn "/app/appdata/bin/bento4/mp4decrypt" "/app/appdata/bin/zlo/static/bento4/mp4decrypt"
+ln -sfn "/app/appdata/bin/shaka_packager/shaka" "/app/appdata/bin/zlo/static/shaka_packager/shaka"
+ln -sfn "/usr/bin/ffmpeg" "/app/appdata/bin/zlo/static/ffmpeg/ffmpeg"
+ln -sfn "/usr/bin/mkvmerge" "/app/appdata/bin/zlo/static/mkvmerge/mkvmerge"
+ln -sfn "/usr/bin/mkvpropedit" "/app/appdata/bin/zlo/static/mkvmerge/mkvpropedit"
+
 # Run migrations if needed
 echo "[entrypoint] Checking for required migrations..."
 gosu "$USER_ID:$GROUP_ID" bash -c "/app/migration_runner.sh"
