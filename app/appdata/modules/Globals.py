d5dcefa17b44951202badf19520cbc1e863733f5
HyperNylium <hypernylium@hnhomelab.com>
2026-07-17 20:59:55 -0400

    ruff fixes and update packages

--- app/appdata/modules/Globals.py
+++ app/appdata/modules/Globals.py
@@ -3,13 +3,13 @@ import threading
 from .LogManager import LogManager
 log_manager = LogManager()
 
-from .FileManager import FileManager  # noqa: E402
+from .FileManager import FileManager  # ruff:ignore[module-import-not-at-top-of-file]
 file_manager = FileManager()
 
-from .QueueManager import QueueManager  # noqa: E402
+from .QueueManager import QueueManager  # ruff:ignore[module-import-not-at-top-of-file]
 queue_manager = QueueManager()
 
-from .RemoteSpecials import RemoteSpecials  # noqa: E402
+from .RemoteSpecials import RemoteSpecials  # ruff:ignore[module-import-not-at-top-of-file]
 remote_specials = RemoteSpecials()
 
 # Global stop event for threads to check and exit gracefully
